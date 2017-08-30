ba3c60eb51c7363d7227a941c25a26b786cca185
shoaib3008757 <shoaibanwar.vu@gmail.com>
2017-08-30 14:49:59 +0500

    one-to-one

---
+++ app/src/main/java/com/ranglerz/realtimechattest/UserDetails.java
@@ -0,0 +1,11 @@
+package com.ranglerz.realtimechattest;
+
+/**
+ * Created by User-10 on 26-Aug-17.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
